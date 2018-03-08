149c053c81ada7a1fbe93d39ba529fc451c1790d
Luiz Santos <97luizsantos@gmail.com>
2018-03-08 14:24:13 -0300

    Adicionando a função de inserir dados

--- lista-encadeada.c
+++ lista-encadeada.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define TAM 10
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct listaDados {
+	int *vetPont;
+	int qtd;
+	int escolha;
+} lista[TAM];
+	
 int menu(); 
+
+void insereDados(struct listaDados *lista);
 
 int main(){
 
@@ -13,7 +22,7 @@ int main(){
 	
 		switch(opcaoSelecionada){
 			case 1 : {
-				insereDados();
+				insereDados(&lista);
 				break;
 			}  
 			
@@ -49,4 +58,27 @@ int menu(){
 	scanf("%d", &opcao);	
 	
 	return opcao;
+}
+
+void insereDados(struct listaDados *lista){
+		
+	int i, j, aux;
+	int a = 0;
+				
+	printf("De 1 a 10, qual vetor voce deseja escolher?");
+	scanf("%d", &aux);
+		
+	aux -= 1;
+	lista[aux].escolha = aux;	
+					
+	printf("Digite a qtd de elementos que voce deseja inserir:");
+	scanf("%d", &lista[aux].qtd);
+	
+	lista[aux].vetPont = (int *) malloc(lista[aux].qtd * sizeof(int));
+		
+	printf("Agora, insira %d numeros no vetor %d\n", lista[aux].qtd, lista[aux].escolha);
+		
+	for(i=0; i<lista[aux].qtd; i++){
+		scanf("%d", &lista[aux].vetPont[i]);
+	} 
 }
