bf4230136fb6b56f00c76d7d96600275464f785d
Luiz Santos <97luizsantos@gmail.com>
2018-03-19 23:49:59 -0300

    Finalizando Menu e as regras de negócios dos itens 1, 2, 3 e 7

--- lista-encadeada.c
+++ lista-encadeada.c
@@ -6,6 +6,7 @@ struct listaDados {
 	int *vetPont;
 	int *vetOrdenado;
 	int qtd;
+	int contador;
 	int escolha;
 } lista[TAM];
 	
@@ -14,6 +15,7 @@ int menu();
 int insereDados(struct listaDados *lista);
 void listarDados(struct listaDados *lista);
 void listarDadosOrdenados(struct listaDados *lista);
+void ordena(int i, struct listaDados *lista);
 void excluirNumero(struct listaDados *lista);
 void aumentarTamanho(struct listaDados *lista);
 void sair(struct listaDados *lista);
@@ -50,6 +52,7 @@ int main(){
 			}
 			
 			case 5 : {
+				system("cls");
 				aumentarTamanho(lista);
 				break;
 			}
@@ -71,9 +74,9 @@ int menu(){
 	int opcao;
 	
 	printf(" --------------- MENU ---------------- \n");
-	printf("1- Inserir Dados\n2- Listar Dados\n3- Listar de Forma Ordenada\n4- Excluir Numero\n5- Aumentar Tamanho\n6-Sair\n");
+	printf("1 - Inserir Numeros\n2 - Listar Numeros\n3 - Listar Numeros de Forma Ordenada\n4 - Listar Todos os Numeros de Forma Ordenada\n5 - Excluir Numero\n6 - Aumentar Tamanho de uma Estrutura\n7 - Sair\n");
 	scanf("%d", &opcao);	
-	
+			
 	return opcao;
 }
 
@@ -87,25 +90,28 @@ int insereDados(struct listaDados *lista){
 	aux -= 1;
 	lista[aux].escolha = aux;	
 	
-	if(lista[aux].vetPont > 0){
-		
-		printf("Estrutura já preenchida");
-		return 0;		
-		
-	} else {					
-	
-		printf("Digite a qtd de elementos que voce deseja inserir:");
-		scanf("%d", &lista[aux].qtd);
-		
+	while(lista[aux].contador == 0){			    
+    	printf("Digite a qtd de elementos que voce deseja inserir:");
+    	scanf("%d", &lista[aux].qtd);
+    	
 		lista[aux].vetPont = (int *) malloc(lista[aux].qtd * sizeof(int));
-			
-		printf("Agora, insira %d numeros no vetor %d\n", lista[aux].qtd, (lista[aux].escolha+1));
-			
-		for(i=0; i<lista[aux].qtd; i++){
-			scanf("%d", &lista[aux].vetPont[i]);
-		} 
+    	
+    	printf("Agora, insira um numero no vetor %d\n", (lista[aux].escolha+1));
+        scanf("%d", &lista[aux].vetPont[0]);
+        
+        lista[aux].contador++;                
+        return 0;
+    } 
+    
+	if(lista[aux].contador == lista[aux].qtd){		
+		printf("Estrutura cheia.");
+		return 0;				
+	} else {		
+		printf("Agora, insira um numero no vetor %d\n", (lista[aux].escolha+1));
+	    scanf("%d", &lista[aux].vetPont[lista[aux].contador]);
+        lista[aux].contador++;		
 	}
-}
+} 
 
 void listarDados(struct listaDados *lista){
 	
@@ -115,14 +121,14 @@ void listarDados(struct listaDados *lista){
 		
 		printf("Posicao: %d\n", i+1);
 		
-		if(lista[i].qtd > 0){
+		if(lista[i].contador > 0){
 			printf("Itens: %d\n", lista[i].qtd);
 			
 			printf("Valores: ");
-			for(j=0; j<lista[i].qtd; j++){
+			for(j=0; j<lista[i].contador; j++){
 				printf("%d", lista[i].vetPont[j]);			
 				
-				if(j < (lista[i].qtd-1)){
+				if(j < (lista[i].contador-1)){
 					printf(" - ");
 				}
 			} 		
@@ -136,47 +142,57 @@ void listarDados(struct listaDados *lista){
 
 void listarDadosOrdenados(struct listaDados *lista){
 
-	int i, j, aux;
-	
+	int i;
+
 	for(i=0; i<TAM; i++){
-		lista[i].vetOrdenado = (int *) malloc(lista[i].qtd * sizeof(int));			
-		for(j=0; j<lista[i].qtd; j++){
-			lista[i].vetOrdenado[j] = lista[i].vetPont[j];	
-		}
-	} 	
+		ordena(i, lista);
+	}
+
+}	
+
+void ordena(int i, struct listaDados *lista){
 	
-	for(i=0; i<TAM; i++){		
-		for(j=i+1; j<lista[i].qtd; j++){	
-			if( lista[i].vetOrdenado[i] > lista[i].vetOrdenado[j]){
-				aux = lista[i].vetOrdenado[i];	
-				lista[i].vetOrdenado[i] = lista[i].vetOrdenado[j];
-				lista[i].vetOrdenado[j] = aux;				
+	int aux, j, c;
+	int *vet;
+	
+	vet = (int *) malloc(lista[i].qtd * sizeof(int));
+	
+	for(j=0; j<lista[i].contador; j++){
+		vet[j] = lista[i].vetPont[j];	
+	}
+	
+	for(j=0; j<lista[i].contador; j++){	
+		for(c=j+1; c<lista[i].contador; c++){		
+			if(vet[j] > vet[c]){
+    			aux = vet[j];
+    			vet[j] = vet[c];
+    			vet[c] = aux;
 			}	
 		}
-	}		
-		
-	for(i=0; i<TAM; i++){
-		printf("Posicao: %d\n", i+1);
-	
-		if(lista[i].qtd > 0){
-			printf("Itens: %d\n", lista[i].qtd);
-				
-			printf("Valores: ");
-			for(j=0; j<lista[i].qtd; j++){
-				printf("%d", lista[i].vetOrdenado[j]);			
-					
-				if(j < (lista[i].qtd-1)){
-					printf(" - ");
-				}
-			} 		
+	}
 			
-		} else {
-			printf("Itens: 0\nValores: 0");
-		}			
+	printf("Posicao: %d\n", i+1);
 		
-		puts("\n\n");
-	} 
-}	
+	if(lista[i].contador > 0){
+		printf("Itens: %d\n", lista[i].qtd);
+			
+		printf("Valores: ");
+		for(j=0; j<lista[i].contador; j++){
+			printf("%d", vet[j]);			
+				
+			if(j < (lista[i].contador-1)){
+				printf(" - ");
+			}
+		} 		
+	} else {
+		printf("Itens: 0\nValores: 0");
+	}	
+		
+	puts("\n");		
+		
+}
+			
+
 
 void excluirNumero(struct listaDados *lista){
 	
@@ -190,7 +206,7 @@ void excluirNumero(struct listaDados *lista){
 	printf("Qual valor deseja apagar?");
 	scanf("%d", &num);		
 	
-	for(j=0; j<lista[vetEscolhido].qtd; j++){
+	for(j=0; j<lista[vetEscolhido].contador; j++){
 		
 		if(lista[vetEscolhido].vetPont[j] == num){
 			lista[vetEscolhido].vetPont[j] = valorNull;
@@ -214,7 +230,7 @@ void aumentarTamanho(struct listaDados *lista){
 	printf("Quantas posicoes deseja aumentar?");
 	scanf("%d", &aumento);	
 	
-	aumento += lista[vetEscolhido].qtd;
+	aumento += lista[vetEscolhido].contador;
 	
 	//tamanho = sizeof(lista[vetEscolhido].vetPont)/sizeof(int);
 	
@@ -227,8 +243,7 @@ void sair(struct listaDados *lista){
 	
 	for(i=0; i<TAM; i++){
 		for(j=0; j<lista[i].qtd; j++){
-			free(lista[i].vetPont);
-			free(lista[i].vetOrdenado);		
+			free(lista[i].vetPont);				
 		}
 	}
 	
